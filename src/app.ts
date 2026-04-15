3c58827549eaf27759f7ac721c958dab437260f6
Luiz Henrique <luiz.h.santos.dev@gmail.com>
2026-04-14 21:14:09 -0300

    feat: add use cases using design patterns

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,22 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import { appRoutes } from './http/routes.js'
+import { ZodError } from 'zod'
+import { env } from './env/index.js'
 
 export const app = fastify()
 
-const prisma = new PrismaClient()
+app.register(appRoutes)
 
-prisma.user.create({
-  data: {
-    name: 'Luiz',
-    email: 'luiz@example.com',
-  },
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error.', issues: error.format() })
+  }
+
+  if (env.NODE_ENV === 'production') {
+    console.error(error)
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' })
 })
